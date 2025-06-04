5d5995d322751de81b40ea948882d597cfb4c4e2
JHONTONCONI <btaedwin123@gmail.com>
2025-06-04 01:04:11 -0400

    AGREGUE EL PRIMER EJECICIO DE SECUENCIALES

---
+++ primer-ejercicio.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+	int b, h, perimetro;
+	cout<<"ingrese el valor de la base"<<endl;
+	cin>>b;
+	cout<<"ingrese el valor de la altura"<<endl;
+	cin>>h;
+	perimetro=b+b+h+h;
+	cout<<"el perimetro es: "<<perimetro<<endl;
+	return 0;
+	
+	
+}
